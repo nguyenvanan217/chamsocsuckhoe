92f22fd6e488b5299bc579f120cd01f0c17df6ab
Ngo Manh Thong <ngomanhthong0812>
2024-07-02 10:36:26 +0700

    update

--- my-project/src/components/register-item-left/RegisterItemLeft.jsx
+++ my-project/src/components/register-item-left/RegisterItemLeft.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../../assets/img/logo1.png';
-import img1 from '../../assets/img/img-bt-1.png';
-import img2 from '../../assets/img/img-bt-2.png';
-import img3 from '../../assets/img/img-bt-3.png';
 import { addUser, fetchUserDatas } from '../../api/apiClient';
 import { Link, useNavigate } from 'react-router-dom';
 function RegisterItemLeft() {
@@ -97,7 +94,7 @@ function RegisterItemLeft() {
                 <div className="flex items-center gap-4 mb-[45px]">
                     <img src={logo} alt="" className="h-[55px] w-auto" />
                     <h1 className="text-[18px] text-[#428BCA] font-[350]">
-                        HỆ THỐNG THÔNG TIN <span className='font-[600]'>TIÊM CHỦNG QUỐC GIA</span>
+                        <span className='font-[600]'> SỐ SỨC KHOẺ GIA ĐÌNH </span>
                     </h1>
                 </div>
                 <div className="flex flex-col gap-3 mb-3">
@@ -154,16 +151,6 @@ function RegisterItemLeft() {
                 <button className='w-full text-white mt-1 text-[15px] font-[500] bg-[#428BCA] px-3 py-2 rounded-[3px]' onClick={() => checkRegister()}>Đăng Ký</button>
                 <div>
                     <h1 className='mt-[20px] text-[15px]'>Bạn đã có tài khoản? <Link to={"/"}><span className='text-[#428BCA]'>Hãy đăng nhập</span></Link></h1>
-                </div>
-            </div>
-            <div>
-                <div className='text-[14px] mt-[100px]'>
-                    Điện thoại hỗ trợ: <span className='font-[500]'>1800 8000 (Nhánh 4)</span>
-                </div>
-                <div className="flex items-center gap-3 w-1/3 my-3 ">
-                    <img src={img1} alt="" className="w-[50%]  object-cover" />
-                    <img src={img2} alt="" className="w-[100%]  object-cover" />
-                    <img src={img3} alt="" className="w-[100%]  object-cover" />
                 </div>
             </div>
         </div>
